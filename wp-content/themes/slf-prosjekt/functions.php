33043db05cc8e2644f3c21f322073d26188b9f03
Benedicte Raae <raae@bgraphic.no>
2015-04-27 11:48:23 +0200

    Initial commit

---
+++ wp-content/themes/slf-prosjekt/functions.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package SLF Project
+ *
+ * Add your custom functions here.
+ */
+
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+function theme_add_before_row_html() {
+  if (is_page_template('project.php')) {
+    return '<div class="hentry"><div class="entry-content">';
+  } else {
+    return '';
+  }
+}
+add_action( 'siteorigin_panels_before_row', 'theme_add_before_row_html' );
+
+function theme_add_after_row_html() {
+  if (is_page_template('project.php')) {
+    return '</div></div>';
+  } else {
+    return '';
+  }
+}
+add_action( 'siteorigin_panels_after_row', 'theme_add_after_row_html' );
+
