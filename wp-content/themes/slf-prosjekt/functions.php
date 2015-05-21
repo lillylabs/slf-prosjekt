4745a494af39ec8982880d6ede401120f426ca90
Benedicte Raae <raae@bgraphic.no>
2015-05-21 12:39:49 +0200

    Overide styling of header image

--- wp-content/themes/slf-prosjekt/functions.php
+++ wp-content/themes/slf-prosjekt/functions.php
@@ -11,6 +11,96 @@ function theme_enqueue_styles() {
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
 /**
+ * Override Twenty Fifteen custom header
+ *
+ */
+
+function twentyfifteen_header_style() {
+	$header_image = get_header_image();
+
+	// If no custom options for text are set, let's bail.
+	if ( empty( $header_image ) && display_header_text() ) {
+		return;
+	}
+
+	// If we get this far, we have custom styles. Let's do this.
+	?>
+	<style type="text/css" id="twentyfifteen-header-css">
+	<?php
+		// Short header for when there is no Custom Header and Header Text is hidden.
+		if ( empty( $header_image ) && ! display_header_text() ) :
+	?>
+		.site-header {
+			padding-top: 14px;
+			padding-bottom: 14px;
+		}
+
+		.site-branding {
+			min-height: 42px;
+		}
+
+		@media screen and (min-width: 46.25em) {
+			.site-header {
+				padding-top: 21px;
+				padding-bottom: 21px;
+			}
+			.site-branding {
+				min-height: 56px;
+			}
+		}
+		@media screen and (min-width: 55em) {
+			.site-header {
+				padding-top: 25px;
+				padding-bottom: 25px;
+			}
+			.site-branding {
+				min-height: 62px;
+			}
+		}
+		@media screen and (min-width: 59.6875em) {
+			.site-header {
+				padding-top: 0;
+				padding-bottom: 0;
+			}
+			.site-branding {
+				min-height: 0;
+			}
+		}
+	<?php
+		endif;
+
+		// Has a Custom Header been added?
+		if ( ! empty( $header_image ) ) :
+	?>
+		.site-branding {
+			background: url(<?php header_image(); ?>) no-repeat 50% 50%;
+            height: <?php echo get_custom_header()->height; ?>px;
+            max-height: 200px;
+            width: <?php echo get_custom_header()->width; ?>px;
+            max-width: 100%;
+            -webkit-background-size: contain;
+			-moz-background-size:    contain;
+			-o-background-size:      contain;
+			background-size:         contain;
+		}
+
+	<?php
+		endif;
+
+		// Has the text been hidden?
+		if ( ! display_header_text() ) :
+	?>
+		.site-title,
+		.site-description {
+			clip: rect(1px, 1px, 1px, 1px);
+			position: absolute;
+		}
+	<?php endif; ?>
+	</style>
+	<?php
+}
+
+/**
  * Style SiteOrigin rows to match theme
  *
  */
