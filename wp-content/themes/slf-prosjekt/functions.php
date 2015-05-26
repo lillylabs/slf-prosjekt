3c69cd85896cd6ce2fe2c848fcdcf01d9e126336
Benedicte Raae <raae@bgraphic.no>
2015-05-26 11:32:04 +0200

    Added angular to theme

--- wp-content/themes/slf-prosjekt/functions.php
+++ wp-content/themes/slf-prosjekt/functions.php
@@ -6,9 +6,20 @@
  */
 
 function theme_enqueue_styles() {
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style(
+		'parent-style',
+		get_template_directory_uri() . '/style.css'
+	);
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+function theme_enqueue_scripts() {
+	wp_enqueue_script(
+		'angularjs',
+		get_stylesheet_directory_uri() . '/js/vendor/angular/angular.min.js'
+	);
+}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_scripts' );
 
 /**
  * Override Twenty Fifteen custom header
