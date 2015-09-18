59535d1c7bc77dd729fcb998f5db00425d0f7097
Simen Schikulski <simenschi@gmail.com>
2015-09-18 16:25:24 +0200

    Laget et enkelt forslag til enklere kalkulator visuelt

--- wp-content/plugins/slf-calculator/slf-calculator-template.php
+++ wp-content/plugins/slf-calculator/slf-calculator-template.php
@@ -2,42 +2,85 @@
 	 ng-controller="SLFCalculatorController"
 	 ng-init='data = <?php $this->the_data_as_JSON() ?>'>
 
+
+<div class="slf-calculator-questions">
+	<h2>Dersom du sykler...</h2>
+
+	<div class="slf-calculator-result-tabs">
+		<span class="slf-calculator-result-tabs-choices">
+			<label>
+				<input type="radio"
+					   name="display"
+					   value="year"
+					   <?php echo $this->is_default_display('year') ? 'checked="true"' : '' ?>
+					   ng-model="data.input.display" />
+				<span>Et år</span>
+			</label>
+			<label>
+				<input type="radio"
+					   name="display"
+					   value="week"
+					   <?php echo $this->is_default_display('week') ? 'checked="true"' : '' ?>
+					   ng-model="data.input.display" />
+				<span>En uke</span>
+			</label>
+			<label>
+				<input type="radio"
+					   name="display"
+					   value="day"
+					   <?php echo $this->is_default_display('day') ? 'checked="true"' : '' ?>
+					   ng-model="data.input.display" />
+				<span>En dag</span>
+			</label>
+		</span>
+	</div>
+
+
+
 	<div class="slf-calculator-form">
-		<div class="slf-calculator-question slf-calculator-question-distance">
-			<h5>Avstand til jobben</h5>
-			<span>Antall km en vei:</span>
-			<span class="spinner">
-				<button ng-click="decrease('distanceToWork')">-</button>
-				<input type="text"
-					   pattern="[0-9]*"
-					   value="<?php $this->the_default_input('distanceToWork') ?>"
-					   ng-model="data.input.distanceToWork" />
-				<button ng-click="increase('distanceToWork')">+</button>
-			</span>
+
+		<div class="slf-sliders">
+
+			<div class="slf-calculator-question slf-calculator-question-distance">
+				<span class="spinner">
+					<button ng-click="decrease('distanceToWork')">-</button>
+					<input type="text"
+						   pattern="[0-9]*"
+						   value="<?php $this->the_default_input('distanceToWork') ?>"
+						   ng-model="data.input.distanceToWork" />
+					<button ng-click="increase('distanceToWork')">+</button>
+				</span>
+				<span class="slf-sliders-label">Km til jobben <small>(en vei)</small></span>
+			</div>
+			
+			<div class="slf-calculator-question slf-calculator-question-frequency">
+				<span class="spinner">
+					<button ng-click="decrease('daysPerWeek')">-</button>
+					<input type="text"
+						   pattern="[0-9]*"
+						   value="<?php $this->the_default_input('daysPerWeek') ?>"
+						   ng-model="data.input.daysPerWeek" />
+					<button ng-click="increase('daysPerWeek')">+</button>
+				</span>
+				<span class="slf-sliders-label">Ganger i uka</span>
+			</div>
+			
+			<div class="slf-calculator-question slf-calculator-question-distance">
+				<span class="spinner">
+					<button ng-click="decrease('weeksPerYear')">-</button>
+					<input type="text"
+						   pattern="[0-9]*"
+						   value="<?php $this->the_default_input('weeksPerYear') ?>"
+						   ng-model="data.input.weeksPerYear" />
+					<button ng-click="increase('weeksPerYear')">+</button>
+				</span>
+				<span class="slf-sliders-label">Uker i året</span>
+			</div>
+
 		</div>
-		<div class="slf-calculator-question slf-calculator-question-frequency">
-			<h5>Hvor ofte vil du sykle?</h5>
-			<span>Antall ganger i uken:</span>
-			<span class="spinner">
-				<button ng-click="decrease('daysPerWeek')">-</button>
-				<input type="text"
-					   pattern="[0-9]*"
-					   value="<?php $this->the_default_input('daysPerWeek') ?>"
-					   ng-model="data.input.daysPerWeek" />
-				<button ng-click="increase('daysPerWeek')">+</button>
-			</span>
-			<span>Antall uker per år:</span>
-			<span class="spinner">
-				<button ng-click="decrease('weeksPerYear')">-</button>
-				<input type="text"
-					   pattern="[0-9]*"
-					   value="<?php $this->the_default_input('weeksPerYear') ?>"
-					   ng-model="data.input.weeksPerYear" />
-				<button ng-click="increase('weeksPerYear')">+</button>
-			</span>
-		</div>
+
 		<div class="slf-calculator-question slf-calculator-question-car-type">
-			<h5>Hvilken bil har du?</h5>
+			<h5>Og bilen din går på</h5>
 			<label>
 				<input type="radio"
 					   name="car"
@@ -60,109 +103,94 @@
 					   value="noCar"
 					   <?php echo $this->is_default_car_type('noCar') ? 'checked="true"' : '' ?>
 					   ng-model="data.input.carType" />
-				<span>Ingen</span>
+				<span>Har ikke</span>
 			</label>
 		</div>
+
 	</div>
+</div>
 
 	<div class="slf-calculator-result">
 
-		<h5 ng-show="data.input.display === 'year'" <?php echo !$this->is_default_display('year') ? 'class="ng-hide"' : '' ?>>
-			Iløpet av et år vil du:</h5>
-		<h5 ng-show="data.input.display === 'week'" <?php echo !$this->is_default_display('week') ? 'class="ng-hide"' : '' ?>>
-			Iløpet av en uke vil du:</h5>
-		<h5 ng-show="data.input.display === 'day'" <?php echo !$this->is_default_display('day') ? 'class="ng-hide"' : '' ?>>
-			På en dag vil du:</h5>
+		<h2 ng-show="data.input.display === 'year'" <?php echo !$this->is_default_display('year') ? 'class="ng-hide"' : '' ?>>
+			Vil du iløpet av et år</h2>
+		<h2 ng-show="data.input.display === 'week'" <?php echo !$this->is_default_display('week') ? 'class="ng-hide"' : '' ?>>
+			Vil du iløpet av en uke</h2>
+		<h2 ng-show="data.input.display === 'day'" <?php echo !$this->is_default_display('day') ? 'class="ng-hide"' : '' ?>>
+			Vil du på en dag</h2>
 
-		<ul>
 
-			<li class="slf-calculator-result-distance-coverd">
-				<span class="slf-calculator-key-word">Sykle</span>
+		<div class="slf-calculator-result-health">
+
+			<div class="slf-calculator-result-health-result slf-calculator-result-distance-coverd">
+				<span class="slf-calculator-title-word"><strong>Sykle</strong></span>
 				<span class="slf-calculator-number" ng-bind="result.distanceCovered() | number : 0">
 					<?php echo number_format ( $this->get_default_km_per_year(), 0 , "," , " " ) ?>
 				</span>
-				<span>kilometer</span>.
-			</li>
+				<span class="slf-calculator-key-word">kilometer</span>
+			</div>
 
-			<li class="slf-calculator-result-minutes-exercised">
-				<span class="slf-calculator-key-word">Mosjonere</span>
+			<div class="slf-calculator-result-health-result slf-calculator-result-minutes-exercised">
+				<span class="slf-calculator-title-word"><strong>Mosjonere</strong></span>
 				<span class="slf-calculator-number" ng-bind="result.minutesExercised() | number : 0">
 					<?php echo number_format ( $this->get_default_calculation("minutesExercised"), 0 , "," , " " ) ?>
 				</span>
-				<span>minutter</span>.
-			</li>
+				<span class="slf-calculator-key-word">minutter</span>
+			</div>
 
-			<li class="slf-calculator-result-calories-burned">
-				<span>Forbrenne</span>
+
+			<div class="slf-calculator-result-health-result slf-calculator-result-calories-burned">
+				<span class="slf-calculator-title-word"><strong>Forbrenne</strong></span>
 				<span class="slf-calculator-number" ng-bind="result.caloriesBurned() | number : 0">
 					<?php echo number_format ( $this->get_default_calculation("caloriesBurned"), 0 , "," , " " ) ?>
 				</span>
-				<span class="slf-calculator-key-word">kalorier</span>.
-			</li>
-		</ul>
+				<span class="slf-calculator-key-word">kalorier</span>
+			</div>
 
-		<h5>Du vil spare miljøet for:</h5>
 
-		<ul>
+		</div>
 
-			<li class="slf-calculator-reduced-co2">
+
+
+
+
+		<div class="slf-calculator-result-earth">
+
+		<h5>Og spare miljøet for</h5>
+
+
+			<div class="slf-calculator-result-earth-result slf-calculator-reduced-co2">
 				<span class="slf-calculator-number" ng-bind="result.reducedCO2Kg() | number : 2">
 					<?php echo number_format ( $this->get_default_calculation("reducedCO2Kg"), 2 , "," , " " ) ?>
 				</span>
-				kg CO2.
-			</li>
+				<span class="slf-calculator-key-word">kg CO2</span>
+			</div>
 
-			<li class="slf-calculator-reduced-nox">
+			<div class="slf-calculator-result-earth-result slf-calculator-reduced-nox">
 				<span class="slf-calculator-number" ng-bind="result.reducedNOXGram() | number : 2">
 					<?php echo number_format ( $this->get_default_calculation("reducedNOXGram"), 2 , "," , " " ) ?>
 				</span>
-				gram NOx.
-			</li>
+				<span class="slf-calculator-key-word">gram NOx</span>
+			</div>
 
-			<li class="slf-calculator-reduced-nox">
+			<div class="slf-calculator-result-earth-result slf-calculator-reduced-nox">
 				<span class="slf-calculator-number" ng-bind="result.reducedDustGram() | number : 2">
 					<?php echo number_format ( $this->get_default_calculation("reducedDustGram"), 2 , "," , " " ) ?>
 				</span>
-				gram svevestøv.
-			</li>
-		</ul>
+				<span class="slf-calculator-key-word">gram svevestøv</span>
+			</div>
 
-		<h5>I tillegg sparer samfunnet<br/>
-			<span class="slf-calculator-number" ng-bind="result.savedNOK() | number : 0">
-				<?php echo number_format ($this->get_default_calculation("savedNOK"), 0 , "," , " " ) ?>
-			</span>
-			kroner.
-		</h5>
 
-		<div class="slf-calculator-result-tabs">
-			<span class="slf-calculator-result-tabs-label">Vis resultat for:</span>
-			<span class="slf-calculator-result-tabs-choices">
-				<label>
-					<input type="radio"
-						   name="display"
-						   value="year"
-						   <?php echo $this->is_default_display('year') ? 'checked="true"' : '' ?>
-						   ng-model="data.input.display" />
-					<span>et år</span>
-				</label>
-				<label>
-					<input type="radio"
-						   name="display"
-						   value="week"
-						   <?php echo $this->is_default_display('week') ? 'checked="true"' : '' ?>
-						   ng-model="data.input.display" />
-					<span>en uke</span>
-				</label>
-				<label>
-					<input type="radio"
-						   name="display"
-						   value="day"
-						   <?php echo $this->is_default_display('day') ? 'checked="true"' : '' ?>
-						   ng-model="data.input.display" />
-					<span>en dag</span>
-				</label>
-			</span>
+
 		</div>
+
+		<div class="slf-calculator-result-costs">
+			<h5>I tillegg sparer samfunnet</h5>
+				<span class="slf-calculator-number">
+					<span ng-bind="result.savedNOK() | number : 0"><?php echo number_format ($this->get_default_calculation("savedNOK"), 0 , "," , " " ) ?></span>,-
+				</span>
+		</div>
+
 
 	</div>
 
